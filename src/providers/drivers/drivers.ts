75ea6586f720a66f842c0ee55a0edb4682f4fb94
mediaotg <esty.mediaotg@gmail.com>
2017-10-18 17:08:07 -0400

    Driver list acting up

--- src/providers/drivers/drivers.ts
+++ src/providers/drivers/drivers.ts
@@ -13,50 +13,10 @@ export class Drivers {
     "about": "Burt is a Bear.",
   };
 
-  // constructor(public api: Api) { }
-
-  constructor(public api: Api) {
-    let items = [
-      {
-        "name": "John Doe",
-        "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Burt is a Bear."
-      },
-      {
-        "name": "Charlie Cheetah",
-        "profilePic": "assets/img/speakers/cheetah.jpg",
-        "about": "Charlie is a Cheetah."
-      },
-      {
-        "name": "Donald Duck",
-        "profilePic": "assets/img/speakers/duck.jpg",
-        "about": "Donald is a Duck."
-      },
-      {
-        "name": "Eva Eagle",
-        "profilePic": "assets/img/speakers/eagle.jpg",
-        "about": "Eva is an Eagle."
-      },
-      {
-        "name": "Ellie Elephant",
-        "profilePic": "assets/img/speakers/elephant.jpg",
-        "about": "Ellie is an Elephant."
-      },
-      {
-        "name": "Molly Mouse",
-        "profilePic": "assets/img/speakers/mouse.jpg",
-        "about": "Molly is a Mouse."
-      },
-      {
-        "name": "Paul Puppy",
-        "profilePic": "assets/img/speakers/puppy.jpg",
-        "about": "Paul is a Puppy."
-      }
-    ];
-
-    for (let item of items) {
-      this.items.push(new Driver(item));
-    }
+  constructor(public api: Api) { 
+    this.query().subscribe(
+      // value => console.log(value)
+    )
   }
 
   query(params?: any) {
